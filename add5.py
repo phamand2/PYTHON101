87f00d2d9bc131ceb53da0b2a649bc045de2448f
Andrew Pham <andrewpham92@gmail.com>
2020-08-18 15:39:21 -0400

    adding examples

---
+++ add5.py
@@ -0,0 +1,10 @@
+# take a user input
+user_number = input('Give me a number: ') # '10'
+
+# add 5 to that input
+result = float(user_number) + 5
+
+# print the result
+
+print(result)
+
---
+++ jokes.py
@@ -0,0 +1,6 @@
+joke = input('Tell me a joke: ')
+
+print('You just said: ')
+print(joke)
+
+print('Wow that is so funny!')
